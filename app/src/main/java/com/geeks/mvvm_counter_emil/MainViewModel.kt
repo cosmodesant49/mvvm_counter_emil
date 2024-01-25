e60bed6a14eb6a8d8b15488fec30a46b31ff0d4a
cosmodesant49 <emilmaldybaev49@gmail.com>
2024-01-25 18:38:02 +0600

    fix

--- app/src/main/java/com/geeks/mvvm_counter_emil/MainViewModel.kt
+++ app/src/main/java/com/geeks/mvvm_counter_emil/MainViewModel.kt
@@ -1,15 +1,18 @@
 package com.geeks.mvvm_counter_emil
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private var counter = 0
 
-    val counterLv: MutableLiveData<Int>
-        get() = _counterLv
+    /*   val counterLv: MutableLiveData<Int>
+           get() = _counterLv
 
+       private val _counterLv = MutableLiveData<Int>()*/
     private val _counterLv = MutableLiveData<Int>()
+    val counterLv: LiveData<Int> get() = _counterLv
 
     fun dec() {
         counter--
